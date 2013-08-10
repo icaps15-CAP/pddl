d8368022475d2f1abf385d98fcd3b56c89d34b5b
guicho <guicho@guicho-ThinkPad-X60.(none)>
2013-08-10 20:11:00 +0900

    testing: adapted the test to the current condition

--- t/matcher.lisp
+++ t/matcher.lisp
@@ -25,54 +25,49 @@
 			 (match pred
 			   ((state 'place 'depot0) t)))
 		       (init *problem*))))
-      (let* ((matches (%try-match p s nil)))
-	(is-false (null s))
-	(is-false (null s2))
-
-	(is (eq (second (parameters s))
-		(first (parameters s3))))
-
-	(iter (for var in (parameters p))
-	      (for obj in (parameters s))
-	      (is (eq (getf matches var)
-		      obj)))
+      (is-false (null s))
+      (is-false (null s2))
+      
+      (is (eq (second (parameters s))
+	      (first (parameters s3))))
+      
+      
+      (let ((matches (%try-match p s nil)))
+	(iter 
+	  (for var in (parameters p))
+	  (for obj in (parameters s))
+	  (is (eq (getf matches var)
+		  obj)))
 
 
 	(signals assignment-error
-	  (%try-match p s2 matches))
-	;; (is (null (%try-match p s2 matches)))
-	))
+	  (%try-match p s2 matches))))
+    
     (is (appliable
 	 (init *problem*)
 	 (action *domain* :drive)))))
 
-(test (all-match-set :depends-on appliability)
-  (let ((set (retrieve-all-match-set
-	       (init depotprob1818)
-	       (action depot :drive))))
-    (is (= (length set) 2))))
-
 (test (apply-action :depends-on appliability)
-  (let ((new-states
-	 (apply-actual-action 
-	  (pddl-intermediate-action
-	   :name 'drive
-	   :domain depot
-	   :problem depotprob1818
-	   :parameters (list
-			(object depotprob1818 'truck1)
-			(object depotprob1818 'depot0)
-			(object depotprob1818 'distributor1)))
-	  (init depotprob1818))))
-    (is-false (null new-states))
-    (handler-bind ((warning #'muffle-warning))
-      (dolist (s new-states)
-	(match s
-	  ((state 'at 'truck1 (where))
-	   (is-false (eq where (object depotprob1818 'depot0))
-		     "truck1 not moved")
-	   (is (eq where (object depotprob1818 'distributor1))
-	       "truck1 moved to the wrong position ~A" where)))))))
+  (let ((aa (pddl-intermediate-action
+	     :name 'drive
+	     :domain depot
+	     :problem depotprob1818
+	     :parameters (list
+			  (object depotprob1818 'truck1)
+			  (object depotprob1818 'depot0)
+			  (object depotprob1818 'distributor1))))
+	(inits (init depotprob1818)))
+    (is (appliable inits aa))
+    (let ((new-states (apply-actual-action aa inits)))
+      (is-false (null new-states))
+      (handler-bind ((warning #'muffle-warning))
+	(dolist (s new-states)
+	  (match s
+	    ((state 'at 'truck1 (where))
+	     (is-false (eq where (object depotprob1818 'depot0))
+		       "truck1 not moved")
+	     (is (eq where (object depotprob1818 'distributor1))
+		 "truck1 moved to the wrong position ~A" where))))))))
 
 (test (simulate-plan :depends-on apply-action)
   (setf env (pddl-environment :plan (pddl-plan :domain depot
