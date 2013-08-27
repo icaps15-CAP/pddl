3e643762f296c7232b63a6f8c77471e919210674
guicho <guicho@guicho-ThinkPad-X60.(none)>
2013-08-27 19:36:15 +0900

    improvement: print-pddl-object became a true printer

--- src/output.lisp
+++ src/output.lisp
@@ -6,11 +6,92 @@
 ;;;; a valid pddl problem, domain etc.
 ;;;;
 
-@export
-(defgeneric print-pddl-object (o))
+(defvar *print-type-p* t)
 
-(defmethod print-pddl-object ((o pddl-domain))
-  `(define (domain ,(name o))
+@export
+(defgeneric print-pddl-object (o &optional s))
+
+(defmethod print-pddl-object :around (o &optional s)
+  (if s
+      (labels 
+	  ((rec (branch s)
+	     (match branch
+	       ((list* (and key (or :function
+				    :predicates 
+				    :init)) rest)
+		(pprint-newline :mandatory s)
+		(pprint-logical-block (s rest :prefix "(" :suffix ")")
+		  (rec key s)
+		  (pprint-newline :mandatory s)
+		  (pprint-indent :block 0 s)
+		  (do () (nil)
+		    (rec (pprint-pop) s)
+		    (pprint-exit-if-list-exhausted)
+		    (write-char #\Space s)
+		    (pprint-newline :mandatory s))))
+	       
+	       ((list* (and key (or :objects
+				    :constants
+				    :types
+				    :requirements
+				    :domain)) rest)
+		(pprint-newline :mandatory s)
+		(pprint-logical-block (s rest :prefix "(" :suffix ")")
+		  (rec key s)
+		  (write-char #\Space s)
+		  (pprint-indent :current 0 s)
+		  (do ((a (pprint-pop) (pprint-pop))
+		       (p nil a)) (nil)
+		    (rec a s)
+		    (pprint-exit-if-list-exhausted)
+		    (write-char #\Space s)
+		    (when (eq p '-)
+		      (pprint-newline :linear s)))))
+
+	       ((list* (and key :action) name rest)
+		(pprint-newline :mandatory s)
+		(pprint-logical-block (s rest :prefix "(" :suffix ")")
+		  (rec key s)
+		  (write-char #\Space s)
+		  (rec name s)
+		  (pprint-indent :block 0)
+		  (pprint-newline :mandatory s)
+		  (do () (nil)
+		    (rec (pprint-pop) s)
+		    (pprint-exit-if-list-exhausted)
+		    (write-char #\Space s)
+		    (rec (pprint-pop) s)
+		    (pprint-exit-if-list-exhausted)
+		    (write-char #\Space s)
+		    (pprint-newline :mandatory s))))
+	       
+	       ((list* (and op (or "AND" "ASSIGN" "OR" "NOT")) rest)
+		(pprint-logical-block (s rest :prefix "(" :suffix ")")
+		  (rec op s)
+		  (write-char #\Space s)
+		  (pprint-indent :current 0 s)
+		  (do () (nil)
+		    (rec (pprint-pop) s)
+		    (pprint-exit-if-list-exhausted)
+		    (write-char #\Space s)
+		    (pprint-newline :mandatory s))))
+	       
+	       ((list* (type atom) _)
+		(pprint-logical-block (s branch :prefix "(" :suffix ")")
+		  (do () (nil)
+		    (rec (pprint-pop) s)
+		    (pprint-exit-if-list-exhausted)
+		    (write-char #\Space s)
+		    (pprint-newline :fill s))))
+	       ((type string) (princ branch s))
+	       (_ (write branch :stream s)))))
+	(rec (call-next-method) s))
+      (call-next-method)))
+
+
+(defmethod print-pddl-object ((o pddl-domain) &optional s)
+  @ignore s
+  `(define (domain ,(print-pddl-object (name o)))
      (:requirements ,@(requirements o))
      (:types ,@(mappend #'print-pddl-object (types o)))
      (:predicates ,@(print-pddl-object (predicates o)))
@@ -20,9 +101,10 @@
      ,@(mapcar #'print-pddl-object (durative-actions o))
      ,@(mapcar #'print-pddl-object (derived-predicates o))))
 
-(defmethod print-pddl-object ((o pddl-problem))
-  `(define (problem ,(name o))
-     (:domain ,(name (domain o)))
+(defmethod print-pddl-object ((o pddl-problem) &optional s)
+  @ignore s
+  `(define (problem ,(print-pddl-object (name o)))
+     (:domain ,(print-pddl-object (name (domain o))))
      (:objects ,@(mappend #'print-pddl-object (objects o)))
      (:init ,@(print-pddl-object (init o)))
      (:goal ,(print-pddl-object (goal o)))
@@ -32,34 +114,54 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; clauses
 
-(defmethod print-pddl-object ((o symbol))
-  o)
+(defmethod print-pddl-object ((o symbol) &optional s)
+  @ignore s
+  (symbol-name o))
 
-(defmethod print-pddl-object ((o list))
-  (walk-tree 
-   (lambda (branch cont)
-     (if (consp branch)
-	 (funcall cont branch)
-	 (print-pddl-object branch)))
-   o))
+(defmethod print-pddl-object ((o list) &optional s)
+  @ignore s
+  (mapcar #'print-pddl-object o))
 
-(defmethod print-pddl-object ((o namable))
-  (name o))
+(defmethod print-pddl-object ((o namable) &optional s)
+  @ignore s
+  (print-pddl-object (name o)))
 
-(defmethod print-pddl-object ((o pddl-predicate))
-  `(,(name o) ,@(mappend #'print-pddl-object (parameters o))))
+(defmethod print-pddl-object ((o pddl-predicate) &optional s)
+  @ignore s
+  `(,(print-pddl-object (name o))
+     ,@(mappend #'print-pddl-object (parameters o))))
 
-(defmethod print-pddl-object ((v pddl-variable))
-  `(,(name v)
-     ,@(when (not (eq (type v) *pddl-primitive-object-type*))
-	     `(- ,(name (type v))))))
+(defmethod print-pddl-object ((o pddl-atomic-state) &optional s)
+  @ignore s
+  (let ((*print-type-p* nil))
+    (call-next-method)))
 
-(defmethod print-pddl-object ((o pddl-action))
-  `(:action ,(name o)
+(defmethod print-pddl-object ((o pddl-function-state) &optional s)
+  @ignore s
+  (let ((*print-type-p* nil))
+    `(= ,(value o) ,(print-pddl-object (body o)))))
+
+
+(defmethod print-pddl-object ((o pddl-variable) &optional s)
+  @ignore s
+  `(,(print-pddl-object (name o))
+     ,@(when (and (not (eq (type o) *pddl-primitive-object-type*))
+		  *print-type-p*)
+	     `(- ,(print-pddl-object (name (type o)))))))
+
+(defmethod print-pddl-object ((o pddl-assign-op) &optional s)
+  @ignore s
+  (%source o))
+
+(defmethod print-pddl-object ((o pddl-action) &optional s)
+  @ignore s
+  `(:action ,(print-pddl-object (name o))
 	    :parameters ,(mappend #'print-pddl-object (parameters o))
 	    :precondition ,(print-pddl-object (precondition o))
 	    :effect ,(print-pddl-object (effect o))))
 
-(defmethod print-pddl-object ((o pddl-actual-action))
-  `(,(name o) ,@(mappend #'print-pddl-object (parameters o))))
+(defmethod print-pddl-object ((o pddl-actual-action) &optional s)
+  @ignore s
+  `(,(print-pddl-object (name o))
+     ,@(mappend #'print-pddl-object (parameters o))))
 
