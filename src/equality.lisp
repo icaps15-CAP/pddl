2515792daa39056b7290ea8cfc8dec190b831706
Masataro Asai <guicho2.71828@gmail.com>
2013-09-13 14:54:58 +0900

    removed duplicated definition of eqstate

--- src/equality.lisp
+++ src/equality.lisp
@@ -6,7 +6,7 @@
   (eq (name a) (name b)))
 
 @export
-(defun eqstate (a b)
-  (and (eqname a b)
-       (tree-equal (parameters a)
-		   (parameters b))))
+(defun eqstate (s1 s2)
+  (and (eqname s1 s2)
+       (equalp (parameters s1)
+	       (parameters s2))))
--- src/goal-check.lisp
+++ src/goal-check.lisp
@@ -13,13 +13,5 @@
        ((notp state) (not (%match-state state states)))))
    (goal problem)))
 
-
-(defun eqstate (s1 s2)
-  @type pddl-atomic-state s1
-  @type pddl-atomic-state s2
-  (and (eqname s1 s2)
-       (equalp (parameters s1)
-	       (parameters s2))))
-
 (defun %match-state (atomic-state states)
   (some (curry #'eqstate atomic-state) states))
