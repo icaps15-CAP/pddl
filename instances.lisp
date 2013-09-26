7f772ca2c96c02e9c8b86de4bf601ff133a061d7
Masataro Asai <guicho2.71828@gmail.com>
2013-09-26 10:32:27 +0900

    added some domains to instanses.lisp

--- instances.lisp
+++ instances.lisp
@@ -5,77 +5,69 @@
 
 (in-package :pddl.instances)
 
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (defun read-many-plans (problem planname-fn)
-    (iter (for i from 1)
-	  (for path = (funcall planname-fn i))
-	  (while (probe-file path))
-	  (for sym = (concatenate-symbols
-		      (name problem) i))
-	  (for plan = (pddl-plan
-		       :domain *domain*
-		       :problem problem
-		       :path path))
-          (eval `(defparameter ,sym ,plan))
-	  (export sym)))
 
-  (defun read-many-problems (problempath-fn planpath-fn)
-    (iter (for i from 1)
-	  (for path = (funcall problempath-fn i))
-	  (while (probe-file path))
-	  (for sym = (parse-file path))
-	  (read-many-plans (symbol-value sym)
-			   (curry planpath-fn i))
-	  (export sym)))
+(defun read-many-plans (problem planname-fn)
+  (iter (for i from 1)
+        (for path = (funcall planname-fn i))
+        (while (probe-file path))
+        (for sym = (concatenate-symbols
+                    (name problem) i))
+        (for plan = (pddl-plan
+                     :domain *domain*
+                     :problem problem
+                     :path path))
+        (eval `(defparameter ,sym ,plan))
+        (export sym)))
 
+(defun read-many-problems (problempath-fn planpath-fn)
+  (iter (for i from 1)
+        (for path = (funcall problempath-fn i))
+        (while (probe-file path))
+        (for sym = (parse-file path))
+        (read-many-plans (symbol-value sym)
+                         (curry planpath-fn i))
+        (export sym)))
 
+(defun data (name)
+  (merge-pathnames
+   name
+   (asdf:system-relative-pathname :pddl "data/")))
 
-  (defun data (name)
-    (merge-pathnames
-     name
-     (asdf:system-relative-pathname :pddl "data/")))
-
+(defun all (domain &rest args)
   (handler-bind ((warning #'muffle-warning))
-    (export (parse-file (data "domain.pddl")))
-    (let ((*domain* (symbol-value 'depot)))
-      (read-many-problems
-       (lambda (i) (data (format nil "pfile~a" i)))
-       (lambda (i j) (data (format nil "pfile~a.plan.~a" i j))))))
+    (let ((sym (parse-file (data domain))))
+      (export sym)
+      (let ((*domain* (symbol-value sym)))
+        (all2 args)))))
 
-  (handler-bind ((warning #'muffle-warning))
-    (export (parse-file (data "costs/domain.pddl")))
-    (let ((*domain* (symbol-value 'cell-assembly)))
-      (read-many-problems
-       (lambda (i) (data (format nil "costs/model2b~a.pddl" i)))
-       (lambda (i j) (data (format nil "costs/model2b~a.plan.~a" i j))))
-      (read-many-problems
-       (lambda (i) (data (format nil "costs/model2a~a.pddl" i)))
-       (lambda (i j) (data (format nil "costs/model2a~a.plan.~a" i j))))))
+(defun all2 (args)
+  (when args
+    (read-many-problems
+     (lambda (i) (data (format nil (first args) i)))
+     (lambda (i j) (data (format nil (second args) i j))))
+    (all2 (nthcdr 2 args))))
 
-  (handler-bind ((warning #'muffle-warning))
-    (export (parse-file (data "costs-eachparts/domain.pddl")))
-    (let ((*domain* (symbol-value 'cell-assembly)))
-      (read-many-problems
-       (lambda (i) (data (format nil "costs-eachparts/model2a-each-~a.pddl" i)))
-       (lambda (i j) (data (format nil "costs-eachparts/model2a-each-~a.plan.~a" i j))))))
-
-  (handler-bind ((warning #'muffle-warning))
-    (export (parse-file (data "woodworking-sat11-strips/domain.pddl")))
-    (let ((*domain* (symbol-value 'woodworking)))
-      (read-many-problems
-       (lambda (i) (data (format nil "woodworking-sat11-strips/p~2,,,'0@a.pddl" i)))
-       (lambda (i j) (data (format nil "woodworking-sat11-strips/p~2,,,'0@a.plan.~a" i j))))))
-  (handler-bind ((warning #'muffle-warning))
-    ;(export (parse-file (data "woodworking-opt11-strips/domain.pddl")))
-    (let ((*domain* (symbol-value 'woodworking)))
-      (read-many-problems
-       (lambda (i) (data (format nil "woodworking-opt11-strips/p~2,,,'0@a.pddl" i)))
-       (lambda (i j) (data (format nil "woodworking-opt11-strips/p~2,,,'0@a.plan.~a" i j))))))
-  
-  (handler-bind ((warning #'muffle-warning))
-    (export (parse-file (data "rovers/domain.pddl")))
-    (let ((*domain* (symbol-value 'rover)))
-      (read-many-problems
-       (lambda (i) (data (format nil "rovers/p~2,,,'0@a.pddl" i)))
-       (lambda (i j) (data (format nil "rovers/p~2,,,'0@a.plan.~a" i j))))))
-  )
+(all "domain.pddl"
+     "pfile~a"
+     "pfile~a.plan.~a")
+(all "costs/domain.pddl"
+     "costs/model2b~a.pddl"
+     "costs/model2b~a.plan.~a"
+     "costs/model2a~a.pddl"
+     "costs/model2a~a.plan.~a")
+(all "costs-eachparts/domain.pddl"
+     "costs-eachparts/model2a-each-~a.pddl"
+     "costs-eachparts/model2a-each-~a.plan.~a")
+(all "woodworking-sat11-strips/domain.pddl"
+     "woodworking-sat11-strips/p~2,,,'0@a.pddl"
+     "woodworking-sat11-strips/p~2,,,'0@a.plan.~a"
+     "woodworking-opt11-strips/p~2,,,'0@a.pddl"
+     "woodworking-opt11-strips/p~2,,,'0@a.plan.~a")
+(all "satellite-typed/domain.pddl"
+     "satellite-typed/p~2,,,'0@a.pddl"
+     "satellite-typed/p~2,,,'0@a.plan.~a"
+     "satellite-typed-loop/p~2,,,'0@a.pddl"
+     "satellite-typed-loop/p~2,,,'0@a.plan.~a")
+(all "rovers/domain.pddl"
+     "rovers/p~2,,,'0@a.pddl"
+     "rovers/p~2,,,'0@a.plan.~a")
