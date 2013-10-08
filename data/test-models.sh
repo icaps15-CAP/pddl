edfb0d94113ee4f04ce52d34ad0990226cb14768
Masataro Asai <guicho2.71828@gmail.com>
2013-10-08 20:12:55 +0900

    use xargs to parallelize it

--- data/test-models.sh
+++ data/test-models.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-PARALLEL=false
+PARALLEL=1
 
-while getopts ":p" opt
+while getopts ":p:" opt
 do
     case ${opt} in
-        p) PARALLEL=true ;;
+        p) PARALLEL=${OPTARG} ;;
         *) echo "unsupported option $opt" ;;
     esac
 done
@@ -13,36 +13,35 @@ done
 shift $(($OPTIND - 1))
 
 if [ $OPT_ERROR ]; then      # option error
-    echo "usage: [-p] directory prefix" >&2
+    echo "usage: [-p NUMPROCS] directory pattern" >&2
     exit 1
 fi
 
-
-echo $0
-echo ${0%%/*}
 SDIR=$(readlink -e ${0%%/*})
-echo $SDIR
+
 
 SCRIPT=$(readlink -e $SDIR/test-problem.sh)
 
 DIR=$(readlink -e $1)
 
-echo $SCRIPT
+# echo $0
+# echo ${0%%/*}
+# echo $SDIR
+# echo $SCRIPT
 echo $DIR
 
 # $1 : フォルダ名
 # $2 : the prefix of each problem
 
-pushd $DIR
-for pddl in $(ls -v $2*.pddl)
-do
-    echo $pddl
-    if $PARALLEL
-    then
-        $SCRIPT $pddl &
-    else
-        $SCRIPT $pddl        
-    fi
-done
-popd
+PATTERN=$2
 
+shift 2
+
+echo Search options : $@
+
+find $DIR -name "$PATTERN" | sort
+echo "find $DIR -name \"$PATTERN\" | sort | xargs -n 1 -P $PARALLEL $SCRIPT $@"
+
+pushd $DIR
+find $DIR -name "$PATTERN" | sort | xargs -n 1 -P $PARALLEL $SCRIPT $@
+popd
