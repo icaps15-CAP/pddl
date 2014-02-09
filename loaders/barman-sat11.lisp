4a67c79459712a2e4b1605c52b8d75bdfb972018
asai <asai@fun-lucy.(none)>
2014-02-09 12:59:06 +0900

    barman-large loads only problem 8x

--- loaders/barman-sat11.lisp
+++ loaders/barman-sat11.lisp
@@ -1,4 +1,4 @@
 (in-package :pddl.instances)
 (all "barman-sat11/domain.pddl"
-     "barman-sat11/pfile.*.pddl"
-     "barman-sat11/large-1.*\.pddl")
+     ;; "barman-sat11/pfile.*.pddl"
+     "barman-sat11/large-8[0-5]-.*\.pddl")
