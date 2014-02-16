c3603b3846388386371b2efb5432e5dc093aedc0
asai <asai@fun-lucy.(none)>
2014-02-16 16:00:20 +0900

    added large3 problems. sometimes solved, sometimes not.

--- data/barman-sat11/fun001.sh
+++ data/barman-sat11/fun001.sh
@@ -2,4 +2,4 @@
 
 scr=../test-models.sh
 opt="-m 15000000 -t 5 -T 21600"
-$scr -S "-v" -p 1 . "^.*/large3.*4\.pddl" $opt
+$scr -S "-v" -p 1 . "^.*/large.*[18]\.pddl" $opt
--- data/barman-sat11/fun003.sh
+++ data/barman-sat11/fun003.sh
@@ -2,5 +2,4 @@
 
 scr=../test-models.sh
 opt="-m 15000000 -t 5 -T 21600"
-$scr -S "-v" -p 1 . "^.*/large2.*[13579]\.pddl" $opt
-# $scr -S "-v" -p 1 . "^.*/large.*[13579]\.pddl" $opt
+$scr -S "-v" -p 1 . "^.*/large3.*1\.pddl" $opt
