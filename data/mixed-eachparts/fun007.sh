4993a8eaee51b3c604e7660d7ea0f2a69446cc81
Masataro Asai <guicho2.71828@gmail.com>
2014-02-18 17:56:16 +0900

    mixed-eachparts, pushing the limit

---
+++ data/mixed-eachparts/fun007.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+scr=../test-models.sh
+opt="-m 15000000 -t 5 -T 21600"
+$scr -S "-v" -p 1 . "^.*/.*[49]\.pddl" $opt
---
+++ data/mixed-eachparts/hosts.py
@@ -0,0 +1,12 @@
+env.hosts=[
+    # "fun000",
+    # "fun001",
+    "fun002",
+    "fun003",
+    "fun004",
+    "fun005",
+    "fun007"
+    ]
+
+env.user="asai"
+env.password="3sokuzenkai"
