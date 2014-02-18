79747a0ece3471f190f2bc0bb997ba8b4652ccac
Masataro Asai <guicho2.71828@gmail.com>
2014-02-18 15:49:36 +0900

    added 5 mixed problems

--- loaders/cell-assembly-eachparts.lisp
+++ loaders/cell-assembly-eachparts.lisp
@@ -1,4 +1,5 @@
 (in-package :pddl.instances)
 
 (all "costs-eachparts/domain.pddl"
-     "costs-eachparts/model2a-each-[0-9]*.pddl")
+     "costs-eachparts/model2a-each-[0-9]*.pddl"
+     "mixed-eachparts/2a2b-mixed-each-[0-9]*.pddl")
