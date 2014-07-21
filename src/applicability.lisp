574a7ac907bf57b840b25e6c9d96354ecacad089
Masataro Asai <guicho2.71828@gmail.com>
2014-07-22 01:17:37 +0900

    safer applicable-condition

--- src/applicability.lisp
+++ src/applicability.lisp
@@ -13,16 +13,19 @@ returns a boolean."
 
 @export
 (defun applicable-condition (atomic-states condition-tree)
+  "This can be improved so that it uses BDD-based tree pruning"
   (walk-tree
    (lambda (clause cont)
      (match clause
        ((andp args) (every #'identity (funcall cont args)))
        ((orp  args) (some  #'identity (funcall cont args)))
        ((notp condition) (not (%match-state condition atomic-states)))
-       (condition             (%match-state condition atomic-states))))
+       ((pddl-atomic-state)   (%match-state clause atomic-states))
+       (_ (warn "numeric precondition is not supported") t)))
    condition-tree))
 
 (defun %match-state (condition atomic-states)
+  "atomic-states, (list atomic-states) -> boolean"
   (some (curry #'eqstate condition) atomic-states))
 
 @export
