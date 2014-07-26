915b5930d2f23dafae2bdce91aa1627093df7cd5
Masataro Asai <guicho2.71828@gmail.com>
2014-07-26 13:37:05 +0900

    gave a clear definition to the simulator API.

--- src/simulator.lisp
+++ src/simulator.lisp
@@ -24,35 +24,53 @@
 
 @export
 (defun proceed (env)
-  "signals an index-out-of-bound error after goal-action"
   (let ((a (elt (actions (plan env)) (index env))))
-    (pddl-environment
-     :domain (domain env)
-     :problem (problem env)
-     :plan (plan env)
-     :index (1+ (index env))
-     :states (apply-ground-action a (states env)))))
+    (values
+     (pddl-environment
+      :domain (domain env)
+      :problem (problem env)
+      :plan (plan env)
+      :index (1+ (index env))
+      :states (apply-ground-action a (states env)))
+     (array-in-bounds-p (actions (plan env)) (1+ (index env))))))
 
 @export
 (defun simulate-plan (env &optional function)
+  " Simulates the plan. Call the function for each state in the plan,
+including the initial state and the goal state.
+
+ The function should take
+two arguments, the first one is the environment and the second one tells if
+it is the goal state. In the goal state, getting (elt (actions plan) (index
+env)) signals an index-out-of-bound error.
+
+ The value i in the slot `index' indicates the state is the i-th state in
+the plan.  The initial state has the index 0."
   (reinitialize-instance env)
-  (handler-case
-      (if (functionp function)
-          (iter (setf env (proceed env))
-                (funcall function env))
-          (iter (setf env (proceed env))))
-    (error () env)))
+  (iter (for (values e safe-next)
+             initially (values env t)
+             then (proceed e))
+        (when (functionp function)
+          (funcall function e safe-next))
+        (while safe-next)
+        (finally (return e))))
 
 @export
-(defmacro with-simulating-plan ((simulation-env start-env) &body body)
+(defmacro with-simulating-plan ((simulation-env
+                                 start-env
+                                 &optional
+                                 (safe-next (gensym "SAFE-NEXT")))
+                                &body body)
+  "See simulate-plan."
   `(simulate-plan ,start-env
-                  (lambda (,simulation-env)
+                  (lambda (,simulation-env ,safe-next)
+                    (declare (ignorable ,safe-next))
                     ,@body)))
 
 @export
 (defun cost (env)
   (if-let ((metric (metric (problem env))))
     (funcall (metric-function metric) (states env))
-    (1- (index env))))
+    (index env)))
 
 
