fb4ec1da1b8b8d03bc39b9e5d5ec32b226751787
Masataro Asai <guicho2.71828@gmail.com>
2014-08-20 01:00:42 +0900

    added medium-size generator

---
+++ data/barman-sat11/middle.lisp
@@ -0,0 +1,20 @@
+(ql:quickload :iterate)
+(ql:quickload :inferior-shell)
+(use-package :iterate)
+(use-package :inferior-shell)
+
+(defun f (start factor i)
+  (floor (+ start (* factor i))))
+
+(iter (for i from 1 to 20)
+      (for cocktail = (f 40 1.25 i))
+      (for ingredients = (f 4 0.3 i))
+      (for shots = (f 35 (* 1.3 1.25) i))
+      (for path = (format
+                   nil
+                   "python barman-generator.py ~a ~a ~a > p~2,,,'0@a.pddl"
+                   cocktail ingredients shots i))
+      (run path :show t))
+
+(exit)
+
