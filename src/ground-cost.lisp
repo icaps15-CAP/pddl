01884410a26a3c53f0c7aa3e2c806ccf28745127
Masataro Asai <guicho2.71828@gmail.com>
2015-01-07 16:51:40 +0900

    bugfix: ground-cost fails on unit-cost instances

--- src/ground-cost.lisp
+++ src/ground-cost.lisp
@@ -6,25 +6,31 @@
 (defun ground-cost (action)
   (shallow-copy
    action
-   :effect `(and ,@(add-list action)
-                 ,@(mapcar (lambda (x) `(not ,x)) (delete-list action))
-                 ,(parse-numeric-effect
-                   `(increase
-                     (total-cost)
-                     ,(reduce #'+
-                              (mapcar (lambda (op)
-                                        (let ((i (increase op)))
-                                          (typecase i
-                                            (pddl-function-state
-                                             (value
-                                              (query-function-state
-                                               i
-                                               (init *problem*))))
-                                            (number i))))
-                                      (assign-ops action)))))
-                 ;; (ematch action
-                 ;;    ((pddl-action
-                 ;;      :domain *domain*
-                 ;;      :problem *problem*)))
-                 )
+   :effect
+   `(and ,@(add-list action)
+         ,@(mapcar (lambda (x) `(not ,x)) (delete-list action))
+         ,@(when (assign-ops action)
+             ;; FIXME this part signals error when parsing (total-cost)
+             ;; while the domain is not an action-cost domain.
+             ;; I am not sure if this `when' statement is sufficient
+             ;; 
+             (list (parse-numeric-effect
+                    `(increase
+                      (total-cost)
+                      ,(reduce #'+
+                               (mapcar (lambda (op)
+                                         (let ((i (increase op)))
+                                           (typecase i
+                                             (pddl-function-state
+                                              (value
+                                               (query-function-state
+                                                i
+                                                (init *problem*))))
+                                             (number i))))
+                                       (assign-ops action)))))))
+         ;; (ematch action
+         ;;    ((pddl-action
+         ;;      :domain *domain*
+         ;;      :problem *problem*)))
+         )
    'assign-ops +unbound+))
